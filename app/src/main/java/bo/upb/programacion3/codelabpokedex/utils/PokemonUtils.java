93d41b582b776627070dedbb2e0d7de4824d90be
Nachin7u7 <ignacio.valencia.sauma@gmail.com>
2020-06-23 20:50:12 -0400

    Tarea de pokedex acabada

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -26,6 +26,18 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+
+        pokemons.add(new Pokemon(13, "Elon Musk", R.drawable.elon_musk, "visionary"));
+        pokemons.add(new Pokemon(14, "Faraon Love Shady", R.drawable.faraon, "Peruvian"));
+        pokemons.add(new Pokemon(15, "Britney Spears", R.drawable.britney, "Carzy Singer"));
+        pokemons.add(new Pokemon(16, "Palomo", R.drawable.palomo, "Terrorist"));
+        pokemons.add(new Pokemon(17, "programer", R.drawable.programer, "Copy-paster from Stackoverflow"));
+        pokemons.add(new Pokemon(18, "spiderman", R.drawable.spider, "hero"));
+        pokemons.add(new Pokemon(19, "corona", R.drawable.corona, "virus"));
+        pokemons.add(new Pokemon(20, "pilfrut", R.drawable.pilfrut, "juice"));
+        pokemons.add(new Pokemon(21, "Saxoman", R.drawable.saxoman, "Crazy Singer"));
+        pokemons.add(new Pokemon(22, "Evo", R.drawable.evo, "donkey"));
+        pokemons.add(new Pokemon(23, "PinkGuy", R.drawable.pink, "Wizzard"));
         return pokemons;
     }
 }
